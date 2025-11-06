cec8a8e5ed425328ea976f5b07fda0e7b30a0c5c
vikeshvrmcse <vikeshkm2002@gmail.com>
2025-11-06 18:06:49 +0530

    Update book backend

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pickle
 import numpy as np
 from flask import Flask, request, jsonify
 
-# Load datasets
+# --- Load datasets ---
 popular_df = pickle.load(open('./models/popular.pkl', 'rb'))
 books = pickle.load(open('./models/books.pkl', 'rb'))
 pt = pickle.load(open('./models/pt.pkl', 'rb'))
@@ -16,11 +16,13 @@ def is_internet_connected():
     response = os.system("ping -c 1 google.com" if os.name != "nt" else "ping -n 1 google.com")
     return response == 0
 
+
 def get_similar_books(book_name):
-    """Return similar books as a list of dictionaries with additional features"""
+    """Return 5 similar books for a given book title"""
     indices = np.where(pt.index == book_name)[0]
     if len(indices) == 0:
         return None
+
     index = indices[0]
     similar_items = sorted(list(enumerate(similarity_score[index])), key=lambda x: x[1], reverse=True)[1:6]
     
@@ -41,13 +43,36 @@ def get_similar_books(book_name):
             data.append(item)
     return data
 
+
+def get_default_books(n=8):
+    """Return top N popular books by default"""
+    data = []
+    default_books = popular_df.head(n)
+    
+    for _, row in default_books.iterrows():
+        item = {
+            "title": row['Book-Title'],
+            "author": row['Book-Author'],
+            "image": row['Image-URL-M'],
+            "publisher": row['Publisher'] if 'Publisher' in row else "N/A",
+            "language": row['Language'] if 'Language' in row else "N/A",
+            "ratings": row['avg-rating'] if 'avg-rating' in row else "N/A",
+            "download_link": row['Download-URL'] if 'Download-URL' in row else "N/A",
+            "buy_link": row['Buy-URL'] if 'Buy-URL' in row else "N/A"
+        }
+        data.append(item)
+    return data
+
+
 # --- Routes ---
 @app.route('/')
 def home():
-    return "Welcome, Tata By By!"
+    return "📚 Welcome to the Book Recommendation API!"
+
 
 @app.route('/BookRecommender', methods=["POST"])
 def book_recommender():
+    """POST: Recommend similar books for a given title"""
     data = request.get_json()
     entered_book_name = data.get("book") if data else None
 
@@ -62,6 +87,17 @@ def book_recommender():
         })
     else:
         return jsonify({"error": "Book not found. Please check the book name and try again."}), 404
+
+
+@app.route('/BookRecommender', methods=["GET"])
+def default_books():
+    """GET: Return top 8 default books"""
+    results = get_default_books(8)
+    return jsonify({
+        "message": "Top 8 popular books",
+        "recommendations": results
+    })
+
 
 # --- Run App ---
 if __name__ == "__main__":
